326444dc9ce1c9d69d837f1570925ecb0b2de773
Nassim <nassim.haddad.pro@gmail.com>
2021-05-17 20:16:45 +0200

    Sidebar + Navbar + HP video

---
+++ client/src/pages/index.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+import HeroSection from '../components/HeroSection';
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+
+const Home = () => {
+  const [isOpen, setisOpen] = useState(false);
+
+  const toggle = () => {
+    setisOpen(!isOpen);
+  }
+
+  return (
+    <>
+      <Sidebar isOpen={isOpen} toggle={toggle}/>
+      <Navbar toggle={toggle}/>
+      <HeroSection/>
+    </>
+  )
+}
+
+export default Home
